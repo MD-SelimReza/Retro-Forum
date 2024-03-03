c15ff6a00f246697dfdca7a1bf5239cf1fcd9da0
MD-SelimReza <rezaselim405@gmail.com>
2024-03-03 14:54:13 +0600

    show latest post successfully

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const postsLoads = async () => {
 const displayPosts = (posts) => {
     const postsContainer = document.getElementById('posts-container');
     posts.forEach(post => {
-        // console.log(post.image);
+        // console.log(post);
         const postBox = document.createElement('div');
         postBox.classList = "flex gap-4 bg-[#F3F3F5] rounded-3xl p-6 mt-5";
         postBox.innerHTML = `
@@ -33,12 +33,58 @@ const displayPosts = (posts) => {
                 <span><i class="fa-regular fa-eye"></i> ${post.view_count}</span>
                 <span><i class="fa-regular fa-clock"></i> ${post.posted_time} min</span>
                 </p>
-                <span class="bg-[#10B981] text-white rounded-full px-1"><i class="fa-regular fa-envelope"></i></span>
+                <span onclick="readPosts(${post.id})" class="bg-[#10B981] text-white rounded-full px-1"><i class="fa-regular fa-envelope"></i></span>
             </div>
         </div>
         `;
         postsContainer.appendChild(postBox);
     });
+};
+
+const readPosts = (post) => {
+    // console.log(post);
 }
+
+const fetchLatestPosts = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const json = await res.json();
+    showLatestPost(json);
+};
+
+const showLatestPost = (latestPosts) => {
+    // console.log(latestPosts);
+    const latestPostContainer = document.getElementById('latestPostContainer');
+    latestPosts.forEach(latestPost => {
+        // console.log(post);
+        const latestPostBox = document.createElement('div');
+        latestPostBox.classList = "card card-compact border p-4 space-y-5";
+        latestPostBox.innerHTML = `
+        <figure class="rounded-2xl">
+            <img
+            src="${latestPost.cover_image}"
+            alt="Shoes"
+            />
+        </figure>
+        <div class="space-y-3">
+            <p><span>${latestPost?.author?.posted_date ? latestPost?.author?.posted_date : 'No published date'}</span></p>
+            <h2 class="card-title text-lg font-extrabold">${latestPost.title}</h2>
+            <p>${latestPost.description}</p>
+            <div class="card-actions">
+                <div class="size-12 rounded-full bg-gray-400">
+                    <img src="${latestPost.profile_image}" alt="profile-pic" class="rounded-full" />
+                </div>
+                <div class="ml-2">
+                    <p class="font-bold">${latestPost?.author?.name}</p>
+                    <p>${latestPost?.author?.designation ? latestPost?.author?.designation : `Unknown`}</p>
+                </div>
+            </div>
+        </div>
+        `;
+        latestPostContainer.appendChild(latestPostBox);
+    })
+}
+
+fetchLatestPosts();
+
 
 postsLoads();
